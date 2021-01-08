f6ed9a8ee8b90a6dc7a6b8080ed40063563780da
Daniel Irwin <dirwin123@gmail.com>
2021-01-08 21:10:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,24 @@
 def using_push(arr, str)
   arr.push(str)
 end 
+
+def using_pop
+  arr.pop 
+  arr
+end
+
+def pop_with_args
+
+end 
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_shift
+  
+end
+
+def shift_with_args
+  
+end
